68321a3c1693cc5ef72dcd37cef4b24e141714a7
gunavardhan27 <bgunavardhan27@gmail.com>
2024-04-27 22:47:48 +0530

    third commit

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
-from base.views import home,register,gaming_profile
+from base.views import home,register,gaming_profile,Login,fun
 urlpatterns = [
     path('',home),
     path('register',register,name='register_form'),
     path('profiles/<str:pk>/',gaming_profile,name="profiles"),
+    path('login/',Login,name='loginpage'),
+    path('done/',fun,name='after_login'),
 ]
 
